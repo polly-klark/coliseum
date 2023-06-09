1cb1170a4db71a6c8e22739a58835712e9050b6c
polly-klark <sapunova.polly@yandex.ru>
2023-06-09 17:35:52 +0300

    begin

---
+++ pythonProject4/client.py
@@ -0,0 +1,105 @@
+from socket import *
+#from scapy.all import *
+import json
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+Form, Window = uic.loadUiType("untitled.ui")
+
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
+
+class Client:
+    def __init__(self, ip, port):
+        self.client = socket(AF_INET, SOCK_STREAM)
+        self.client.connect(
+            (ip, port)
+        )
+    def connect(self):
+        try:
+            msg = self.client.recv(1024).decode('utf-8')
+        except Exception as e:
+            print(f'ERROR: {str(e)}')
+            exit()
+
+        # if msg == 'YOU ARE CONNECTED!':
+        #     self.listen('')
+        # else:
+        #     exit()
+
+    def sender(self, text):
+        self.client.send(text.encode('utf-8'))
+        while self.client.recv(1024).decode('utf-8') != 'got':
+            self.client.send(text.encode('utf-8'))
+
+    def data_receiver(self):
+        return self.client.recv(1024).decode("utf-8")
+
+    # def listen(self,req):
+    #     is_work = True
+    #     while is_work:
+    #         if req:
+    #             if req == 'disconnect':
+    #                 self.sender(req)
+    #                 print(self.client.recv(1024).decode('utf-8'))
+    #
+    #             else:
+    #                 self.sender(req)
+    #                 data = json.loads(
+    #                     self.client.recv(1024).decode('utf-8')
+    #                 )
+    #
+    #                 if data['answer']:
+    #                     print(f'SERVER ANSWER:\n\t{data["answer"]}')
+    #                 elif data['error']:
+    #                     print(f'SERVER ERROR:\n\t{data["error"]}')
+
+#Client(input('Type server ip: '), 1111).connect()
+
+def podkl(_str):
+    ip = form.lineEdit.text()
+    global cl
+    cl = Client(ip, 1111)
+    cl.connect()
+    form.label_3.setText('Статус: подключено')
+
+def otkl(_str):
+    obj = dict()
+    obj["type"] = "disconnect"
+    req = json.dumps(obj)
+    cl.sender(req)
+    form.label_3.setText('Статус: не подключено')
+
+def zapros(_str):
+    obj = dict()
+    obj["type"] = "request"
+    param = dict()
+    if form.radioButton.isChecked():
+        param["file"] = "background"
+    elif form.radioButton_2.isChecked():
+        param["file"] = "attack"
+    elif form.radioButton_3.isChecked():
+        param["file"] = "modified"
+    obj["param"] = param.copy()
+    req = json.dumps(obj)
+    cl.sender(req)
+
+    is_work = True
+    while is_work:
+        try:
+            data = dict(json.loads(cl.data_receiver()))
+            if data["type"] == "request_answer":
+                for i in data["param"]["files"]:
+                    form.listWidget.addItem(str(data["param"]["files"][i]))
+        except Exception as e:
+            data = ''
+            is_work = False
+
+form.pushButton.clicked.connect(lambda: podkl("string"))
+form.pushButton_7.clicked.connect(lambda: otkl("string"))
+form.pushButton_2.clicked.connect(lambda: zapros("string"))
+
+app.exec_()
---
+++ pythonProject4/server.py
@@ -0,0 +1,84 @@
+import os
+from socket import *
+#from scapy.all import *
+import sqlite3 as sql
+import json
+
+class Server:
+    def __init__(self, ip, port, base_name):
+        print(f'SERVER IP: {ip}\nSERVER PORT: {port}\n')
+        self.data_name = base_name
+        self.server = socket(AF_INET, SOCK_STREAM)
+        self.server.bind(
+            (ip, port)
+        )
+        self.server.listen(25)
+
+    def sender(self, user, text):
+        user.send(text.encode('utf-8'))
+
+    def start_server(self):
+        while True:
+            user, addr = self.server.accept()
+            print(f'CONNECTED:\n\tIP: {addr[0]}\n\tPORT: {addr[1]}')
+            self.listen(user, addr)
+
+    def listen(self, user, addr):
+        self.sender(user, 'YOU ARE CONNECTED!')
+        is_work = True
+
+        while is_work:
+            try:
+                data = user.recv(1024)
+                self.sender(user, 'got')
+            except Exception as e:
+                data = ''
+                is_work = False
+
+            if len(data) > 0:
+
+                msg = dict(json.loads(data.decode('utf-8')))
+
+                if msg["type"] == "disconnect":
+                    self.sender(user, 'YOU ARE DISCONNECTED!')
+                    user.close()
+                    print(f'DISCONNECTED: \n\tIP: {addr[0]}')
+                    is_work = False
+                elif msg["type"] == "request":
+                    if msg["param"]["file"] == "background":
+                        ans = dict()
+                        ans["type"] = "request_answer"
+                        param = dict()
+                        files = os.listdir('C:/Users/sapun/PycharmProjects/pythonProject4/background')
+                        param["files"] = files.copy()
+                        ans["param"] = param.copy()
+                        s = json.dumps(ans)
+
+                    # con = sql.connect(self.data_name)
+                    # cur = con.cursor()
+                    #
+                    # try:
+                    #     answer = [x for x in cur.execute(msg)]
+                    #     error = ''
+                    # except Exception as e:
+                    #     error = str(e)
+                    #     answer = ''
+                    #
+                    # con.commit()
+                    # cur.close()
+                    # con.close()
+
+                    # ans = json.dumps(
+                    #     { 'answer' : answer, 'error' : error }
+                    # )
+
+                    self.sender(user, s)
+
+                data = b''
+                msg = ''
+
+            else:
+                print('DISCONNECTED!')
+                is_work = False
+
+Server('192.168.119.1', 1111, 'data.db').start_server()
