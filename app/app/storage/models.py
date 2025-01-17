cc86defa757720b000bd61cd2303061176a37b54
polly-klark <sapunova.polly@yandex.ru>
2025-01-17 11:36:29 +0300

    my little project

--- app/app/storage/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Boolean
-from storage.database import Base
-
-class AppConfig(Base):
-    __tablename__ = "config"
-
-    id = Column(Integer, primary_key=True, index=True)
-    key = Column(String)
-    value = Column(String)
--- app/app/storage/schemas.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class AppConfigBase(BaseModel):
-    key: str
-    value: str
