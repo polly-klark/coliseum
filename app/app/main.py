8cb941591a7f55afd92fb980cfd1e8d9349434ba
polly-klark <sapunova.polly@yandex.ru>
2025-01-17 18:44:06 +0300

    my little project

--- app/app/main.py
+++ app/app/main.py
@@ -8,7 +8,8 @@ import gridfs
 from datetime import datetime, timedelta
 from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorClient
-from passlib.context import CryptContext
+from passlib.context import CryptContext # type: ignore
+import gostcrypto
 import jwt
 
 # Настройки приложения
@@ -30,7 +31,8 @@ class User(BaseModel):
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    password = password.encode('cp1251')
+    return gostcrypto.gosthash.new('streebog256', data=password).hexdigest()
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
