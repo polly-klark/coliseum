a17feea6c8a307f43844c741060f547f6c41bd8d
polly-klark <sapunova.polly@yandex.ru>
2025-01-20 17:20:21 +0300

    begin fixing

--- app/app/main.py
+++ app/app/main.py
@@ -2,51 +2,53 @@ import os
 from fastapi import FastAPI, UploadFile, File, HTTPException, Depends
 from fastapi.responses import StreamingResponse
 from fastapi.security import OAuth2PasswordRequestForm
-from pymongo import MongoClient
+# from pymongo import MongoClient, database
 from pydantic import BaseModel, Field
-import gridfs
+# import gridfs
 from datetime import datetime, timedelta
 from bson import ObjectId
-from motor.motor_asyncio import AsyncIOMotorClient
-from passlib.context import CryptContext 
-import gostcrypto
-import jwt
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorGridFSBucket
+# from passlib.context import CryptContext 
+import gostcrypto, jwt, secrets
+from datetime import datetime, timezone
 
 # Настройки приложения
 app = FastAPI()
 
 # Подключение к MongoDB
 client = AsyncIOMotorClient('mongodb://localhost:27017/')
-db = client['mydatabase']
-fs = gridfs.GridFS(db)
+auth_db = client['test']
+db = client['test_gridfs']
+# print(isinstance(db, database.Database))  # Должно вернуть True
+fs = AsyncIOMotorGridFSBucket(db)
 
 # Модель пользователя
 class User(BaseModel):
     id: str = Field(default_factory=lambda: str(ObjectId()))
     username: str
     hashed_password: str
-    role: str  # Добавляем поле для роли
+    is_admin: bool  # Добавляем поле для роли
 
-# Настройки для хеширования паролей
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+# # Настройки для хеширования паролей
+# pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def hash_password(password: str) -> str:
     password = password.encode('cp1251')
     return gostcrypto.gosthash.new('streebog256', data=password).hexdigest()
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return plain_password == hashed_password
 
 # JWT настройки
-SECRET_KEY = "your_secret_key"
+SECRET_KEY = secrets.token_hex(32)  # Генерирует 64-значный шестнадцатеричный ключ
 ALGORITHM = "HS256"
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -55,7 +57,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
 @app.post("/register")
 async def register(user: User):
     user.hashed_password = hash_password(user.hashed_password)
-    await db.users.insert_one(user.dict())
+    await db.users.insert_one(user.model_dump())
     return {"message": "User registered successfully"}
 
 # Вход пользователя и получение токена
@@ -64,14 +66,14 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user = await db.users.find_one({"username": form_data.username})
     
     if not user or not verify_password(form_data.password, user['hashed_password']):
-        raise HTTPException(status_code=400, detail="Invalid credentials")
+        raise HTTPException(status_code = 400, detail = "Invalid credentials")
     
     access_token_expires = timedelta(minutes=30)
     
     # Включаем роль пользователя в полезную нагрузку токена
     access_token = create_access_token(
-        data={"sub": user['username'], "role": user['role']}, 
-        expires_delta=access_token_expires
+        data = {"sub": user['username'], "is_admin": user['is_admin']}, 
+        expires_delta = access_token_expires
     )
     
     return {"access_token": access_token, "token_type": "bearer"}
@@ -80,10 +82,10 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     if file.filename == '':
-        raise HTTPException(status_code=400, detail="Имя файла отсутствует")
+        raise HTTPException(status_code = 400, detail = "File has not name")
 
     # Сохраняем файл в GridFS
-    fs.put(file.file, filename=file.filename)
+    fs.put(file.file, filename = file.filename)
     
     return {"message": "Файл загружен", "filename": file.filename}
 
@@ -93,10 +95,10 @@ async def get_file(filename: str):
     file = fs.find_one({'filename': filename})
     
     if file is None:
-        raise HTTPException(status_code=404, detail="Файл не найден")
+        raise HTTPException(status_code = 404, detail = "File not found")
     
     # Возвращаем файл с правильным именем
-    return StreamingResponse(file, media_type='application/octet-stream', headers={"Content-Disposition": f"attachment; filename={file.filename}"})
+    return StreamingResponse(file, media_type = 'application/octet-stream', headers = {"Content-Disposition": f"attachment; filename={file.filename}"})
 
 # Получаем список файлов
 @app.get("/files")
@@ -113,7 +115,7 @@ async def delete_file(filename: str):
     file_info = fs.find_one({'filename': filename})
     
     if file_info is None:
-        raise HTTPException(status_code=404, detail="Файл не найден")
+        raise HTTPException(status_code = 404, detail="File not found")
     
     # Удаляем файл по его ID
     fs.delete(file_info._id)
--- app/app/storage/creds.js
+++ app/app/storage/creds.js
@@ -1,4 +1,4 @@
 db.users.insertMany([
-    {"name": "admin", "passw": "admin"},
-    {"name": "user", "passw": "user"}
+    {"username": "admin", "hashed_password": "44f213f36bb3395a56907df0493c7d38e843ee24c9588178b043115f882d9ff7", "is_admin": "True"},
+    {"username": "user", "hashed_password": "21197c64e75efd10473356d55a47ef722c7232a237f62c2609fd3ffcf0cf2fbe", "is_admin": "False"}
     ])
