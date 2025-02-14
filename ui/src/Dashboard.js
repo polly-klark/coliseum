0b1fc960dda2f34f0a1f52dfab077ec17128eae6
polly-klark <sapunova.polly@yandex.ru>
2025-02-14 19:10:29 +0300

    start antd

--- ui/src/Dashboard.js
+++ ui/src/Dashboard.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
+import { Button, Input } from "antd";
+import "./App.css"; // Импорт вашего CSS файла
 
 const Dashboard = ({ token }) => {
-    const [data, setData] = React.useState(null);
-    const fetchData = async (role) => {
-        const response = await axios.get(`http://localhost:8000/${role}`, {headers: { Authorization: `Bearer ${token}` },});
+  const [data, setData] = React.useState(null);
+  const fetchData = async (role) => {
+    const response = await axios.get(`http://localhost:8000/${role}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
     setData(response.data);
-    };
-    return (
-        <div>
-            <button onClick={() => fetchData('admin')}>Get Admin Data</button>
-            <button onClick={() => fetchData('user')}>Get User Data</button>
-            {data && <div>{JSON.stringify(data)}</div>}
-        </div>
-    );
+  };
+  return (
+    <div>
+      <button onClick={() => fetchData("admin")}>Get Admin Data</button>
+      <button onClick={() => fetchData("user")}>Get User Data</button>
+      {data && <div>{JSON.stringify(data)}</div>}
+    </div>
+  );
 };
 
 export default Dashboard;
