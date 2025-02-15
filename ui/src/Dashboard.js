7f89e671a9943a6a94c27e7199f3690677c25c84
polly-klark <sapunova.polly@yandex.ru>
2025-02-15 09:49:29 +0300

    home page

--- ui/src/Dashboard.js
+++ ui/src/Dashboard.js
@@ -14,11 +14,31 @@ const Dashboard = ({ token }) => {
   return (
     <>
       <div className="home_container">
-        <Button onClick={() => fetchData("modified")}>Ваши атаки</Button>
-        <Button onClick={() => fetchData("attack")}>Шаблоны атак</Button>
-        <Button onClick={() => fetchData("background")}>Фоновый трафик</Button>
+        <Button
+          color="primary"
+          variant="outlined"
+          onClick={() => fetchData("modified")}
+        >
+          Ваши атаки
+        </Button>
+        <Button
+          color="danger"
+          variant="outlined"
+          onClick={() => fetchData("attack")}
+        >
+          Шаблоны атак
+        </Button>
+        <Button
+          color="purple"
+          variant="outlined"
+          onClick={() => fetchData("background")}
+        >
+          Фоновый трафик
+        </Button>
       </div>
-      <div className="home_container">{data ? <div>{JSON.stringify(data)}</div> : <p>Нет данных</p>}</div>
+      <div className="home_container">
+        {data ? <div>{JSON.stringify(data)}</div> : <p>Нет данных</p>}
+      </div>
     </>
   );
 };
