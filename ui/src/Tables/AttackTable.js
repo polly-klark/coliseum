8a7dc77108edbc77525117454ea70d7443a6912f
polly-klark <sapunova.polly@yandex.ru>
2025-02-18 19:15:59 +0300

    begin modal

--- ui/src/Tables/AttackTable.js
+++ ui/src/Tables/AttackTable.js
@@ -1,12 +1,14 @@
 import React from "react";
 import dayjs from "dayjs";
-import { Table, Space, message } from "antd";
+import { Table, Space, message, Modal, Form, Button } from "antd";
 import "../App.css"; // Импорт вашего CSS файла
 import axios from "axios";
 
 const { Column } = Table;
 
 const AttackTable = ({ data, user, token, fetchData }) => {
+  const [loading, setLoading] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
   const handleDelete = async (filename, event) => {
     event.preventDefault(); // Предотвращаем переход по ссылке
 
@@ -25,53 +27,92 @@ const AttackTable = ({ data, user, token, fetchData }) => {
     }
   };
 
+  const showModal = () => {
+    setOpen(true);
+  };
+
   const handleModification = async (filename, event) => {
     event.preventDefault(); // Предотвращаем переход по ссылке
-
-    
-  }
+    showModal();
+  };
+  const handleOk = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      setOpen(false);
+    }, 3000);
+  };
+  const handleCancel = () => {
+    setOpen(false);
+  };
 
   return (
-    <Table dataSource={data} rowKey="filename">
-      <Column
-        title="№ п/п"
-        key="number"
-        render={(text, record, index) => index + 1}
-      />
-      <Column title="Имя файла" dataIndex="filename" key="filename" />
-      <Column title="Размер файла" dataIndex="length" key="length" />
-      <Column
-        title="Дата загрузки"
-        dataIndex="upload_date"
-        key="upload_date"
-        render={(text, record) =>
-          dayjs(record.upload_date).format("DD.MM.YYYY HH:mm")
-        }
-      />
-      <Column
-        title="Действие"
-        key="action"
-        render={(_, record) => (
-          <Space size="middle">
-            <a>Запустить {record.lastName}</a>
-            <a
-              href="#"
-              onClick={(event) => handleModification(record.filename, event)}
-            >
-              Модифицировать
-            </a>
-            {user === "admin" && (
+    <>
+      <Table dataSource={data} rowKey="filename">
+        <Column
+          title="№ п/п"
+          key="number"
+          render={(text, record, index) => index + 1}
+        />
+        <Column title="Имя файла" dataIndex="filename" key="filename" />
+        <Column title="Размер файла" dataIndex="length" key="length" />
+        <Column
+          title="Дата загрузки"
+          dataIndex="upload_date"
+          key="upload_date"
+          render={(text, record) =>
+            dayjs(record.upload_date).format("DD.MM.YYYY HH:mm")
+          }
+        />
+        <Column
+          title="Действие"
+          key="action"
+          render={(_, record) => (
+            <Space size="middle">
+              <a>Запустить {record.lastName}</a>
               <a
                 href="#"
-                onClick={(event) => handleDelete(record.filename, event)}
+                onClick={(event) => handleModification(record.filename, event)}
               >
-                {/* Предотвращаем переход по ссылке */}Удалить
+                Модифицировать
               </a>
-            )}
-          </Space>
-        )}
-      />
-    </Table>
+              {user === "admin" && (
+                <a
+                  href="#"
+                  onClick={(event) => handleDelete(record.filename, event)}
+                >
+                  {/* Предотвращаем переход по ссылке */}Удалить
+                </a>
+              )}
+            </Space>
+          )}
+        />
+      </Table>
+      <Modal
+        open={open}
+        title="Title"
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={[
+          <Button key="back" onClick={handleCancel}>
+            Return
+          </Button>,
+          <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
+            Submit
+          </Button>,
+          <Button
+            key="link"
+            href="https://google.com"
+            target="_blank"
+            type="primary"
+            loading={loading}
+            onClick={handleOk}
+          >
+            Search on Google
+          </Button>,
+        ]}
+      ></Modal>     
+    </>
   );
 };
 
