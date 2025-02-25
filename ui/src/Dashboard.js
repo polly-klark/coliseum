471d8d0639ddd98f1e954e1f4f7ddc96ad26bee0
polly-klark <sapunova.polly@yandex.ru>
2025-02-25 16:40:52 +0300

    with upload drag

--- ui/src/Dashboard.js
+++ ui/src/Dashboard.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button, Divider, Modal, Space } from "antd";
+import { Button, Divider, Modal, Space, Upload, message } from "antd";
+import { InboxOutlined } from "@ant-design/icons";
 import "./App.css"; // Импорт вашего CSS файла
 import ModTable from "./Tables/ModTable";
 import AttackTable from "./Tables/AttackTable";
 import BgTable from "./Tables/BgTable";
+
+const { Dragger } = Upload;
+
+const props = {
+  name: "file",
+  multiple: true,
+  action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload",
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (status === "done") {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  onDrop(e) {
+    console.log("Dropped files", e.dataTransfer.files);
+  },
+};
 
 const Dashboard = ({ token }) => {
   const [user, setUser] = useState(null);
@@ -165,7 +188,20 @@ const Dashboard = ({ token }) => {
           </Button>,
         ]}
       >
-        <div>Здесь будет содержимое модального окна</div>
+        <div>
+          <Dragger {...props}>
+            <p className="ant-upload-drag-icon">
+              <InboxOutlined />
+            </p>
+            <p className="ant-upload-text">
+              Click or drag file to this area to upload
+            </p>
+            <p className="ant-upload-hint">
+              Support for a single or bulk upload. Strictly prohibited from
+              uploading company data or other banned files.
+            </p>
+          </Dragger>
+        </div>
       </Modal>
     </>
   );
