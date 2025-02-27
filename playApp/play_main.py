d87a4df7e849d8198d1239a764812337c7ba3343
polly-klark <sapunova.polly@yandex.ru>
2025-02-27 12:11:26 +0300

    start play

--- playApp/play_main.py
+++ playApp/play_main.py
@@ -1,3 +1,9 @@
 import os, tempfile
 from subprocess import check_output
 from fastapi import FastAPI, UploadFile, File
+
+app = FastAPI()
+
+@app.get("/hello")
+async def get_hello():
+    return("Hello world!")
