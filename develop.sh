d463826dc218a63cfeb4a2690a705eb0058c4b56
polly-klark <sapunova.polly@yandex.ru>
2025-05-31 10:50:00 +0300

    somthing in develop

--- develop.sh
+++ develop.sh
@@ -20,6 +20,6 @@ source /venv/bin/activate && uvicorn main:app --host 0.0.0.0 --port 8000 --log-l
 #Запускаем celery
 #source /venv/bin/activate && watchmedo auto-restart --directory=/app --pattern=*.py --recursive -- celery -A tasks worker --loglevel=INFO &
 #Устанавливаем модули React
-npm --prefix /ui install
+#npm --prefix /ui install
 #Запускаем фронтенд
 REACT_APP_API_URL="http://localhost:8000/api/" npm --prefix /ui start
