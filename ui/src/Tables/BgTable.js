40dbc29e41f03d1448aa86ae3df9bcc9174ed2dc
polly-klark <sapunova.polly@yandex.ru>
2025-09-08 18:05:10 +0300

    download background

--- ui/src/Tables/BgTable.js
+++ ui/src/Tables/BgTable.js
@@ -30,11 +30,18 @@ const BgTable = ({ data, user, token, fetchData }) => {
     event.preventDefault(); // Предотвращаем переход по ссылке downloadbackground
 
     try {
-      await axios.get(`http://localhost:8000/downloadbackground/${filename}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
+      const response = await axios.get(`http://localhost:8000/downloadbackground/${filename}`, {
+        headers: { Authorization: `Bearer ${token}` },
+        responseType: 'blob'
       });
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', filename);
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+      window.URL.revokeObjectURL(url);
       message.success(`Файл "${filename}" успешно передан на скачивание`);
     } catch (error) {
       console.error("Ошибка при получении файла:", error);
