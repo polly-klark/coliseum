0ca616a74aff30966bce6d6015f615f100252c88
polly-klark <sapunova.polly@yandex.ru>
2025-10-25 17:19:44 +0300

    ip front

--- ui/src/Login.js
+++ ui/src/Login.js
@@ -14,7 +14,7 @@ const Login = ({ setToken }) => {
     const { username, password } = values; // Извлекаем значения из объекта
     try {
       const response = await axios.post(
-        "http://localhost:8000/login",
+        "http://192.168.42.129:8000/login",
         new URLSearchParams({
           username,
           password,
